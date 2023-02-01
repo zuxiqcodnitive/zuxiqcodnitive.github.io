a0e3b07cc3867137a79c2e7f45f82b03cb2f3a73
zuxiqcodnitive <pelivanxi@protonmail.com>
2023-02-01 13:47:15 +0200

    add old project

---
+++ src/pages/contact.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ScrollToTop from "../components/scroll-to-top";
+import SEO from "../components/seo";
+import ContactContainer from "../containers/contact";
+import NewsletterArea from "../containers/global/newsletter";
+import PageBanner from "../containers/global/page-banner";
+import Footer from "../layouts/footer";
+import Header from "../layouts/header";
+import Layout from "../layouts/index";
+
+const ContactPage = () => {
+    return (
+        <React.Fragment>
+            <Layout>
+                <SEO title="ZUXIQ CODNITIVE Contact" />
+                <div className="wrapper">
+                    <Header />
+                    <PageBanner
+                        title="Contact us"
+                        excerpt="Pleasure rationally encounter consequences <br />
+                        are extremely painful great oppurtunity"
+                        image="./images/contact/1.png"
+                    />
+                    <ContactContainer />
+                    <Footer />
+                    <ScrollToTop />
+                </div>
+            </Layout>
+        </React.Fragment>
+    );
+};
+
+export default ContactPage;
